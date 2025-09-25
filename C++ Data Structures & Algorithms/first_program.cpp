3caa26ad1d196463b2628c90b090e479d6393fc7
Priyanshu Lakum <lakumpriyanshu12@gmail.com>
2025-09-25 18:41:32 +0530

    day_2

--- C++ Data Structures & Algorithms/first_program.cpp
+++ C++ Data Structures & Algorithms/first_program.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout << "hello world\n";
+int main() {
+    float a, b; // Use float for decimals
+
+    cout << "Enter number a: ";
+    cin >> a;
+
+    cout << "Enter number b: ";
+    cin >> b;
+
+    // Print the label first, then the calculation
+    cout << "Addition is: " << a + b << "\n";
+    cout << "Subtraction is: " << a - b << "\n";
+    cout << "Division is: " << a / b << "\n";
+    cout << "Multiplication is: " << a * b << "\n";
+
     return 0;
 }
