98175e9132c95353282b0c997819179ad5a2b358
Priyanshu Lakum <lakumpriyanshu12@gmail.com>
2025-11-16 22:58:32 +0530

    Added C++ programs

---
+++ C++ Data Structures & Algorithms/vectors.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+     vector<int> nums{4,1,2,1,2};
+        int ans=0;
+        for(int val : nums){
+            ans+=val;
+        }
+    cout << "sum=" << ans<< endl;
+    return 0;
+}
